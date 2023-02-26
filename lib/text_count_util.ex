50c3a8a3b5d4d2a52b1387668117108231c235b5
Konstantin Shilov <shilovk@gmail.com>
2023-02-26 09:10:31 +0300

    init

---
+++ lib/text_count_util.ex
@@ -0,0 +1,18 @@
+defmodule TextCountUtil do
+  @moduledoc """
+  Documentation for `TextCountUtil`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TextCountUtil.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/text_count_util_test.exs
@@ -0,0 +1,8 @@
+defmodule TextCountUtilTest do
+  use ExUnit.Case
+  doctest TextCountUtil
+
+  test "greets the world" do
+    assert TextCountUtil.hello() == :world
+  end
+end
